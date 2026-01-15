a66584ba67f483f0d2859d6ceaa134436e53a2ac
Mjettt <miklopsiq@gmail.com>
2026-01-15 14:34:16 +0100

    moj pierwszy commit

---
+++ main.js
@@ -0,0 +1,4 @@
+    function pokazInfo() {
+        document.getElementById("info").innerHTML =
+            "Lubię JavaScript, bo umożliwia dynamiczne działania na stronie bez jej przeładowania!";
+    }
